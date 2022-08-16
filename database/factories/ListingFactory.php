12e1b487701e00bc89eebf3969fa7c5d4c649e56
mukstoo <rodrigogreconunes@gmail.com>
2022-08-15 22:56:27 -0300

    end project

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'tags' => 'laravel, api, backend',
+            'company' => fake()->company(),
+            'email' => fake()->companyEmail(),
+            'website' => fake()->url(),
+            'location' => fake()->city(),
+            'description' => fake()->paragraph(5),
+
+            /* 'name' => fake()->name(),
+            'email' => fake()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10), */
+        ];
+    }
+}
